d0a0a8b05e050b72dc98cc514299e3bbbcf3ce9a
giskook <zhangkai.gis@163.com>
2014-10-24 15:20:10 +0800

    all properties are basically well\!

--- src/kmap.js
+++ src/kmap.js
@@ -28,6 +28,10 @@ var kMap = {
 
     levels : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
 
+    worldpixels : [256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824],
+
+    worldsidetiles : [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304],
+
     //    20037508.3427892 * 2 / 256 / (Math.pow(2,level));
     resolution : [156543.03392804062,78271.51696402031,39135.758482010155,
         19567.879241005077,9783.939620502539,4891.969810251269,2445.9849051256347,
@@ -49,6 +53,21 @@ var kMap = {
         right :  0,
         bottom : 0
     },
+
+    viewtiles : {
+        level : 12,
+        startx : 0,
+        starty : 0,
+        endx : 0,
+        endy : 0
+    },
+
+    pixeloutside : {
+        height : 0,
+        width : 0
+    },
+
+    domain : "http://192.168.1.166",
 
     setPixelBounds : function(left, top, right, bottom){
         this.pixelbounds.left = left;
@@ -82,7 +101,25 @@ var kMap = {
         return this.geobounds;
     },
 
-    getTileURL : function(index){
+    getViewTiles : function () {
+        this.viewtiles.level = this.level;
+        this.viewtiles.startx = Math.floor(((this.geobounds.left - this.worldbounds.left) / this.resolution[this.level]) / 256);
+        this.viewtiles.starty = Math.floor(((this.worldbounds.top - this.geobounds.top) / this.resolution[this.level]) / 256);
+        this.viewtiles.endx = Math.ceil(((this.geobounds.right - this.worldbounds.left) / this.resolution[this.level]) / 256);
+        this.viewtiles.endy = Math.ceil(((this.geobounds.top - this.worldbounds.top) / this.resolution[this.level]) / 256);
+
+        return this.viewtiles;
+    },
+
+    getPixelOutSide : function(){
+        this.pixeloutside.width = ((this.geobounds.left - this.worldbounds.left) / this.resolution[this.level]) % 256;
+        this.pixeloutside.height = ((this.worldbounds.top - this.geobounds.top) / this.resolution[this.level]) % 256;
+
+        return this.pixelbounds;
+    },
+
+    getTileURL : function(){
+
     }
 };
 
--- src/map_test.js
+++ src/map_test.js
@@ -8,15 +8,23 @@
 //        document.write(kMap.resolution[i]);
 //        document.write(" ");
 //    }
-    kMap.setMap("kmap", {x:2,y:2}, 2);
-    document.writeln(kMap.level);
 
-    document.writeln(kMap.name);
+//    kMap.setMap("kmap", {x:2,y:2}, 2);
+//    document.writeln(kMap.level);
+//
+//    document.writeln(kMap.name);
+//
+//    document.writeln(kMap.center.x);
+//    document.writeln(kMap.center.y)
+//
+//    var four = kMap.center.x + kMap.center.y;
+//    document.writeln(four);
 
-    document.writeln(kMap.center.x);
-    document.writeln(kMap.center.y)
+//    for(var i in kMap.levels){
+//        document.writeln(Math.pow(2,i)*256);
+//    }
 
-    var four = kMap.center.x + kMap.center.y;
-    document.writeln(four);
-    document.writeln(127/2);
+    for(var i in kMap.worldsidetiles){
+        document.writeln(kMap.worldsidetiles[i]*kMap.worldsidetiles[i]);
+    }
 })();
