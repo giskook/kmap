ba2d3cccb1c89296f120df67e9c5518de040c213
giskook <zhangkai.gis@163.com>
2014-11-04 08:35:33 +0800

    insert img to div

--- src/kmapcontrol.js
+++ src/kmapcontrol.js
@@ -4,9 +4,11 @@
 
 $(document).ready(function(){
     var bigImg = document.createElement("img");     //创建一个img元素
-    bigImg.width = "256px";  //200个像素 不用加px
-    bigImg.height = "256px";
-    bigImg.src = "http://www.baidu.com/img/bdlogo.gif";   //给img元素的src属性赋值
-    document.getElementById('div1').appendChild(bigImg);      //为dom添加子元素img
+    bigImg.width = "256";
+    bigImg.height = "256";
+    bigImg.style.left = 1000;
 
-});
+    bigImg.src = "http://192.168.1.155/images/1.png";
+    document.getElementById('div1').appendChild(bigImg);
+
+ });
