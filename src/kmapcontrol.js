f6b1b99f8f66d1fd3fa9a9341de15be6adb84647
giskook <zhangkai.gis@163.com>
2014-11-07 13:28:10 +0800

    kMapControl.js setImg

--- src/kmapcontrol.js
+++ src/kmapcontrol.js
@@ -1,16 +1,31 @@
 /**
  * Created by z on 2014/10/31.
  */
+var kMapControl = {
+    setImg : function(imgproperty){
+        var divcontainer = imgproperty.container;
+        var div = document.createElement("div");
+        var img = document.createElement("img");
+        document.getElementById(divcontainer).appendChild(div);
+        div.appendChild(img);
+
+        img.style.width  = imgproperty.width;
+        img.style.height = imgproperty.height;
+        img.style.position = "absolute";
+        img.style.top  = imgproperty.top;
+        img.style.left = imgproperty.left;
+        img.src = imgproperty.src;
+    }
+};
 
 $(document).ready(function(){
-    var bigImg = document.createElement("img");     //创建一个img元素
-    bigImg.style.width  = '256px';
-    bigImg.style.height = '256px';
-
-    bigImg.style.position = 'absolute';
-    bigImg.style.top  = "0px";
-    bigImg.style.left = "0px";
-    bigImg.src = "http://online1.map.bdimg.com/tile/?qt=tile&x=0&y=0&z=4&styles=pl&udt=20141102";
-    document.getElementById('div1').appendChild(bigImg);
+    kMapControl.setImg({
+        container:"div1",
+        width:"256px",
+        height:"256px",
+        top:"0px",
+        left:"0px",
+        src:"http://online1.map.bdimg.com/tile/?qt=tile&x=0&y=0&z=4&styles=pl&udt=20141102"
+    });
 
  });
