60715e20c4f672b281fbaeb58774a3c61f6e7066
GisKook <zhangkai.gis@163.com>
2014-11-08 07:52:05 +0800

    testing kMapControl.setMap

--- src/kmapcontrol.js
+++ src/kmapcontrol.js
@@ -15,17 +15,55 @@ var kMapControl = {
         img.style.top  = imgproperty.top;
         img.style.left = imgproperty.left;
         img.src = imgproperty.src;
+    },
+
+    setMap : function(mapproperty){
+        var container = mapproperty.container;
+        var level = mapproperty.level;
+        var center = mapproperty.center;
+        var pixelbounds = mapproperty.pixelbounds;
+        kMap.setMap(container, center, level);
+        kMap.setPixelBounds(pixelbounds.left, pixelbounds.top,pixelbounds.right, pixelbounds.bottom);
+        var geobounds  = kMap.getViewBounds();
+        var viewtiles = kMap.getViewTiles();
+        var pixeloutside = kMap.getPixelOutSide();
+        document.write(viewtiles.startx);
+        document.write(" ");
+        document.write(viewtiles.endx);
+        document.write(" ");
+        document.write(viewtiles.starty);
+        document.write(" ");
+        document.write(viewtiles.endy);
+        document.write(" ");
+
+        /*for(var i = viewtiles.startx; i < viewtiles.endx; ++i){
+            for(var j = viewtiles.starty; j < viewtiles.endy; ++j){
+                kMapControl.setImg({
+                    container : container,
+                    width : "256px",
+                    height : "256px",
+                    top : (j*256-pixeloutside.height)+"px",
+                    left : (i*256-pixeloutside.width)+"px",
+                    src : "http://online1.map.bdimg.com/tile/?qt=tile&x=0&y=0&z=4&styles=pl&udt=20141102"
+                })
+            }
+        }*/
     }
 };
 
 $(document).ready(function(){
-    kMapControl.setImg({
-        container:"div1",
-        width:"256px",
-        height:"256px",
-        top:"0px",
-        left:"0px",
-        src:"http://online1.map.bdimg.com/tile/?qt=tile&x=0&y=0&z=4&styles=pl&udt=20141102"
-    });
-
+    kMapControl.setMap({
+        container : "div1",
+        level : 20,
+        center : {
+            x : 127,
+            y : 34
+        },
+        pixelbounds : {
+            left : document.documentElement.clientLeft,
+            top : document.documentElement.clientTop,
+            right : document.documentElement.clientLeft + document.documentElement.clientWidth,
+            bottom : document.documentElement.clientTop + document.documentElement.clientHeight
+        }
+    })
  });
